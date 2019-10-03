c484cf275245c5331b1d259b8b2f73a10be858d1
Germán González <german.gonzalez@abstracta.com.uy>
2019-10-02 23:57:58 -0300

    First version

--- Program.cs
+++ Program.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+
 using Services;
+
+using System.Threading.Tasks;
+
 using Uruguay;
 
 namespace webirBackend
