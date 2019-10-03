52c21c6c7ff112448452a46ff524fafd487cbf09
Germán González <german.gonzalez@abstracta.com.uy>
2019-10-03 00:11:57 -0300

    Added services project

---
+++ Services/ApiClients.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Uruguay;
+
+namespace Services
+{
+    public class ApiClients
+    {
+        public static async System.Threading.Tasks.Task<ExecuteResponse> getValueAsync(short coinCode)
+        {
+            wsbcucotizacionesin uy = new wsbcucotizacionesin();
+            uy.Moneda = new short[] { coinCode };
+            uy.FechaDesde = DateTime.Now.Date;
+            uy.FechaHasta = DateTime.Now.Date;
+            
+            wsbcucotizacionesSoapPortClient client = new wsbcucotizacionesSoapPortClient();
+
+            await client.OpenAsync();
+            ExecuteResponse response = await client.ExecuteAsync(uy);
+
+            return response;
+        }
+    }
+}
